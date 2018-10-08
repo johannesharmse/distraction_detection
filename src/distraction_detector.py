461081dd351609a38cfe000ff84d9ecc2f3023b4
johannesharmse <johannesharmse91@gmail.com>
2018-10-07 20:45:27 -0700

    Write to video

--- src/distraction_detector.py
+++ src/distraction_detector.py
@@ -1,11 +1,11 @@
 from keras.preprocessing.image import img_to_array
 import imutils
-import cv2 as cv
+import cv2
 from keras.models import load_model
 import numpy as np
 
-face_cascade = cv.CascadeClassifier('haarcascade_files/haarcascade_frontalface_default.xml')
-eye_cascade = cv.CascadeClassifier('haarcascade_files/haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_files/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_files/haarcascade_eye.xml')
 distract_model = load_model('cnn/distraction_model.hdf5', compile=False)
 
 # frame
@@ -18,65 +18,86 @@ min_size_h_eye = 60
 scale_factor = 1.1
 min_neighbours = 5
 
-prediction_classes = ['distract', 'focus']
+# Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
+fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+video_out = cv2.VideoWriter('video_out.avi', fourcc, 10.0,(1200, 900))
 
 # video streaming
-cv.namedWindow('Watcha Looking At?')
-camera = cv.VideoCapture(0)
+cv2.namedWindow('Watcha Looking At?')
+camera = cv2.VideoCapture(0)
+
+# Check if camera opened successfully
+if (camera.isOpened() == False): 
+    print("Unable to read camera feed")
+
 while True:
-    frame = camera.read()[1]
+    ret, frame = camera.read()
 
-    frame = imutils.resize(frame,width=frame_w)
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    if ret:
+        frame = imutils.resize(frame,width=frame_w)
 
-    frameClone = frame.copy()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray,scaleFactor=scale_factor,minNeighbors=min_neighbours,minSize=(min_size_w,min_size_h),flags=cv.CASCADE_SCALE_IMAGE)
-    
-    if len(faces) > 0:
         frameClone = frame.copy()
 
-        for (x,y,w,h) in faces:
-            cv.rectangle(frameClone,(x,y),(x+w,y+h),(255,0,0),2)
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = frameClone[y:y+h, x:x+w]
-            eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor=scale_factor,minNeighbors=min_neighbours,minSize=(min_size_w_eye,min_size_w_eye))
+        faces = face_cascade.detectMultiScale(gray,scaleFactor=scale_factor,minNeighbors=min_neighbours,minSize=(min_size_w,min_size_h),flags=cv2.CASCADE_SCALE_IMAGE)
+        
+        if len(faces) > 0:
+            frameClone = frame.copy()
 
-            probs = list()
-            label = list()
+            for (x,y,w,h) in faces:
+                cv2.rectangle(frameClone,(x,y),(x+w,y+h),(255,0,0),2)
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = frameClone[y:y+h, x:x+w]
+                eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor=scale_factor,minNeighbors=min_neighbours,minSize=(min_size_w_eye,min_size_w_eye))
 
-            for (ex,ey,ew,eh) in eyes:
-                cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),border_w)
-                roi = roi_color[ey+border_w:ey+eh-border_w, ex+border_w:ex+ew-border_w]
+                probs = list()
+                label = list()
 
-                roi = cv.resize(roi, (64, 64))
-                roi = roi.astype("float") / 255.0
-                roi = img_to_array(roi)
-                roi = np.expand_dims(roi, axis=0)
+                for (ex,ey,ew,eh) in eyes:
+                    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),border_w)
+                    roi = roi_color[ey+border_w:ey+eh-border_w, ex+border_w:ex+ew-border_w]
 
-                # print(roi)
+                    roi = cv2.resize(roi, (64, 64))
+                    roi = roi.astype("float") / 255.0
+                    roi = img_to_array(roi)
+                    roi = np.expand_dims(roi, axis=0)
 
-                prediction = distract_model.predict(roi)
+                    # print(roi)
+
+                    prediction = distract_model.predict(roi)
+                    # print(prediction)
+                    probs.append(prediction[0])
+                    # label.append(prediction_classes[prediction.argmax()])
+
+                probs_mean = np.mean(probs)
+
+                if probs_mean <= 0.5:
+                    label = 'distracted'
+                else:
+                    label = 'focused'
+
+                cv2.putText(frameClone,label,(x,y-5), cv2.FONT_HERSHEY_SIMPLEX, 
+                1, (0,0,255), 3, cv2.LINE_AA)
                 # print(prediction)
-                probs.append(prediction[0])
-                # label.append(prediction_classes[prediction.argmax()])
+                # print(label)
+        
+        # Write the frame into the file 'output.avi'
+        video_out.write(frameClone)
 
-            probs_mean = np.mean(probs)
+        print(frameClone.shape)
 
-            if probs_mean <= 0.5:
-                label = 'distracted'
-            else:
-                label = 'focussed'
+        cv2.imshow('Watcha Looking At?', frameClone)
 
-            cv.putText(frameClone,label,(x,y-5), cv.FONT_HERSHEY_SIMPLEX, 
-            1, (0,0,255), 3, cv.LINE_AA)
-            # print(prediction)
-            # print(label)
+        
 
-    cv.imshow('Watcha Looking At?', frameClone)
+        # quit with q
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    if cv.waitKey(1) & 0xFF == ord('q'):
+    else:
         break
 
 camera.release()
-cv.destroyAllWindows()
+video_out.release()
+cv2.destroyAllWindows()
